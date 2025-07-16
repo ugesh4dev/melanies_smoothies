7bc23e39a903c8058ac884fc470cdcf5be1d7a17
Ugesh <2781161+ugesh4dev@users.noreply.github.com>
2025-07-16 15:54:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,8 @@ if ingredient_list:
     st.text(ingredient_list)
     ingredients_string = ''
     for each_fruit in ingredient_list:
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', each_fruit,' is ', search_on, '.')
         ingredients_string += each_fruit + ' '
         smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/"+each_fruit)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
