c29f1c78b034532237b339bd68feb845540cfe4d
Ugesh <2781161+ugesh4dev@users.noreply.github.com>
2025-07-16 15:57:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 #session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.stop()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 # Convert the Snowpark Dataframe to Pandas Dataframe so that we can use the LOC function
